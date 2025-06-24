7128d4b9efe0165505d30bccf90895e62dda9e84
Wictor Zhao <wictor_x@hotmail.com>
2025-06-24 15:02:12 +0200

    Update README and remove unused variables

--- variables.tf
+++ variables.tf
@@ -1,39 +1,3 @@
-variable "role_name" {
-  description = "The name of the IAM role used by GitHub Actions."
-  type        = string
-  default     = "github_actions_assume_role"
-}
-
-variable "allowed_s3_write_arns" {
-  description = "S3 bucket ARNs the role can write to."
-  default     = []
-  type        = list(string)
-}
-
-variable "allowed_s3_read_arns" {
-  description = "S3 bucket ARNs the role can read from."
-  default     = []
-  type        = list(string)
-}
-
-variable "allowed_ecr_arns" {
-  description = "ECR repository ARNs the role can publish images to."
-  default     = []
-  type        = list(string)
-}
-
-variable "allowed_ecs_arns" {
-  description = "ECS clusters ARNs the role can update task definitions and services of."
-  default     = ["*"]
-  type        = list(string)
-}
-
-variable "github_repositories" {
-  description = "A list of repositories that the OIDC role can access"
-  default     = []
-  type        = list(string)
-}
-
 variable "github_org" {
   type        = string
   description = "The GitHub organization that owns the repositories."
