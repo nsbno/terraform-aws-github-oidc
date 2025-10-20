803985c2188d3b5d7f1b1b6d59f9cba262811d9f
Wictor Zhao <wictor_x@hotmail.com>
2025-10-20 12:00:17 +0200

    Add support for allowed GitHub repositories

--- variables.tf
+++ variables.tf
@@ -14,3 +14,9 @@ variable "github_oidc_provider_arn" {
   description = "The ARN of the GitHub OIDC provider. Used to migrate older implementations of GHA."
   default     = null
 }
+
+variable "github_repo_list_to_allow" {
+  type        = list(string)
+  description = "List of GitHub repositories that are allowed to deploy to the specified environment. Format: owner/repo"
+  default     = []
+}
